c83e33ef1da9a2d318679ed0d0a6f05e238fa280
Pedro <pcordista368@gmail.com>
2020-04-21 18:36:37 -0300

    Upload

---
+++ src/components/Display/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,.6)',
+        alignItems: 'flex-end',
+        width: '100%'
+    },
+    displayValue: {
+        fontSize: 60,
+        color: 'white'
+    }
+})
+
+export default props => 
+<View style={styles.display}>
+<Text style={styles.displayValue} numberOfLines={1}>{props.value}</Text>
+</View>
